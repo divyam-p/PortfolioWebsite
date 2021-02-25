505f20f69cc74427bb22bdb84b69e86d79cf0637
Divyam Patel <70924790+divyam-p@users.noreply.github.com>
2021-02-24 21:15:59 -0500

    Added particles to website.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -7,10 +7,27 @@ import AboutMe from './AboutMe.js'
 import Experiences from './Experiences.js'
 import experiences from './defaultExperiences.js'
 import { BrowserRouter } from 'react-router-dom' 
+import Particles from 'react-particles-js' 
+import { SiCircle } from 'react-icons/si'
+import { HiPlusCircle } from 'react-icons/hi'
 
 const App = () => {
   return (
     <div className="main">
+      <div className="particles"> 
+        <Particles 
+        params={{
+            		particles: {
+            			line_linked: {
+            				shadow: {
+            					enable: true,
+            					color: "#3CA9D1",
+            					blur: 5
+            				}
+            			}
+            		}
+            	}}/>
+      </div>
       <BrowserRouter>
         <Header />
         <AboutMe id="about"/>
